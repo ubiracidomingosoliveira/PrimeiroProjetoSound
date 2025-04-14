0ae3fd9ce892675673e8a2d5072c0aa07cc9dcee
UbiraciOliveira <ubira.domingos@gmail.com>
2025-04-14 20:21:19 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Soud";
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas + "\n");
+
+}
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("Digite 1 para registra uma banda\n");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas\n");
+    Console.WriteLine("Digite 3 para avaliar uma banda\n");
+    Console.WriteLine("digite 4 para a média de uma banda\n");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2:
+            Console.WriteLine("Você escolheu a opção" + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("Você Escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchal :)");
+            break;
+        default: Console.WriteLine("Opção inválida!");
+            break;
+    }
+    void RegistrarBanda()
+    {
+        Console.Clear();
+        Console.WriteLine("Registro de bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirLogo();
+    }
+    
+}
+ExibirLogo();
+ExibirOpcoesDoMenu();
